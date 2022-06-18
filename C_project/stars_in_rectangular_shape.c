d9286e4127347f08e11dd04a90be321ff630ae3b
kizitoamandi90@gmail.com <kizitoamandi90@gmail.com>
2022-06-19 00:34:43 +0100

    program that prints star in rectangular shape

---
+++ C_project/stars_in_rectangular_shape.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+// This codes prints stars in rectangular pattern
+int i, j;
+for (i = 1; i <= 6; i++)
+{
+	for(j = 1; j <= 6; j++)
+	{
+            printf("* ");
+	}
+printf("\n");
+}
+return (0);
+}
